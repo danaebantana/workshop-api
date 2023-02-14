1236cb7a15b8c933e78e4b6e909800785d3ac418
danaebantana <danae.bantana@hotmail.com>
2023-02-14 22:31:40 +0200

    added new and create actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :redirect_if_not_signed_in, only: [:new]
+
   def show
     @post = Post.find(params[:id])
   end
@@ -16,6 +18,21 @@ class PostsController < ApplicationController
     posts_for_branch(params[:action])
   end
 
+  def new
+    @branch = params[:branch]
+    @categories = Category.where(branch: @branch)
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      redirect_to root_path
+    end
+  end
+
   private
 
   # Return data for the specific page, depending on the action’s name
@@ -30,5 +47,10 @@ class PostsController < ApplicationController
       category: params[:category],
       branch: params[:action]
     }).call
+  end
+
+  def post_params
+    params.require(:post).permit(:content, :title, :category_id)
+                         .merge(user_id: current_user.id)
   end
 end
