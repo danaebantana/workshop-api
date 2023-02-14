20e3226b9f987f388253bd200453a4fd41f98c86
danaebantana <danae.bantana@hotmail.com>
2023-02-14 21:10:58 +0200

    defined a default_scope for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   belongs_to :category
+
+  # orders posts in descending order by the creation date
+  default_scope -> { includes(:user).order(created_at: :desc) }
 end
