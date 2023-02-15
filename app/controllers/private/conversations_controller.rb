0a02496db859d2cb11530968e4ab983bdc1bea26
danaebantana <danae.bantana@hotmail.com>
2023-02-15 19:21:03 +0200

    added conversation controller functions

--- app/controllers/private/conversations_controller.rb
+++ app/controllers/private/conversations_controller.rb
@@ -1,7 +1,7 @@
 class Private::ConversationsController < ApplicationController
   def create
     recipient_id = Post.find(params[:post_id]).user.id
-    conversation = Private::Conversation.new(sender_id: current_user.id, 
+    @conversation = Private::Conversation.new(sender_id: current_user.id, 
                                              recipient_id: recipient_id)
     if conversation.save
       Private::Message.create(user_id: recipient_id, 
@@ -9,11 +9,23 @@ class Private::ConversationsController < ApplicationController
                               body: params[:message_body])
       respond_to do |format|
         format.js {render partial: 'posts/show/contact_user/message_form/success'}
+        add_to_conversations unless already_added?
       end
     else
       respond_to do |format|
         format.js {render partial: 'posts/show/contact_user/message_form/fail'}
       end
     end
+  end
+
+  private
+
+  def add_to_conversations
+    session[:private_conversations] ||= []
+    session[:private_conversations] << @conversation.id
+  end
+
+  def already_added?
+    session[:private_conversations].include?(@conversation.id)
   end
 end
