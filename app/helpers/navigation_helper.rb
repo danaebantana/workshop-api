d8f60a5bf8e5b165b6db6935cf18c8d889254f34
danaebantana <danae.bantana@hotmail.com>
2023-02-16 13:27:26 +0200

    added method that sets the header partials to be used

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -6,4 +6,17 @@ module NavigationHelper
       'layouts/navigation/collapsible_elements/non_signed_in_links'
     end
   end
+
+  # render the navigation header's content
+  def nav_header_content_partials
+    partials = []
+    if params[:controller] == 'messengers'
+      partials << 'layouts/navigation/header/messenger_header'
+    else # controller is not messengers
+      partials << 'layouts/navigation/header/toggle_button'
+      partials << 'layouts/navigation/header/home_button'
+      partials << 'layouts/navigation/header/dropdowns' if user_signed_in?
+    end
+    partials
+  end
 end
